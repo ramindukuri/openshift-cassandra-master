0ce1ba964fa8d521b0e267f2f284c8c529ac490d
ram.indukuri <ram@knoldus.com>
2018-12-06 12:10:16 -0500

    custom

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift:latest
+CMD [ "sleep", "10000" ]
--- build.sh
+++ build.sh
@@ -20,3 +20,14 @@
 
 oc new-build --strategy docker --binary --docker-image openshift-cassandra:latest --name waus
 oc start-build openshift-cassandra --from-dir . --follow
+
+
+docker build . -t openshift-cassandra:latest
+oc new-build --strategy docker --binary --docker-image openshift-cassandra:latest --name openshift-cassandra
+oc start-build openshift-cassandra:latest --from-dir . --follow
+
+
+sudo rm -rf openshift.local.clusterup 
+docker build . -t oc-cassandra
+docker tag oc-cassandra:latest rindukuri/oc-cassandra
+docker push rindukuri/oc-cassandra
